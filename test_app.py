1554ca1176fa1c1136ac424cede4a04202927d5f
Pravin Rathod <pravin.rathod@MacBookPro.lan>
2025-09-06 18:28:10 +0530

    Initial commit - next commit

--- test_app.py
+++ test_app.py
@@ -1,7 +1,8 @@
 from app import app
 
 def test_home():
-	client = app.test_client()
-	response = client.get("/")
-	assert response.status_code == 200
-	assert b"Hello, CI/CD World!" in response.data
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Hello, CI/CD World!" in response.data
+
