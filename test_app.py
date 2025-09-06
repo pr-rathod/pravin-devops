45aef0268e481400482bd80271ba9e5ff5036b3f
Pravin Rathod <pravin.rathod@MacBookPro.lan>
2025-09-06 18:30:23 +0530

    Initial commit - update test_app

--- test_app.py
+++ test_app.py
@@ -4,5 +4,5 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Hello, CI/CD World!" in response.data
+    assert "Hello, CI/CD World!" in response.data
 
